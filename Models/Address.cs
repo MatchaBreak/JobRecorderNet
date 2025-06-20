0c0f9bb256507bf0208275bf2d781b3754702ae3
Lucas Setiady <luca.setia@gmail.com>
2025-06-20 13:26:17 +1000

    Added cshtml pages for Navigation Bar

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,9 +1,11 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace JobRecorderNet.Models
 {
     public class Address
     {
-        [Key] 
+        [Key]
         public int Id { get; set; }
 
         [ForeignKey("ClientId")]
